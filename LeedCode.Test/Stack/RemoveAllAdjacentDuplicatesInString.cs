00873ddbd60343573c574c0b317a1d46c4b0d8b3
JuMaxi <jmaximovski@gmail.com>
2024-04-03 18:11:53 +0100

    RemovingAdjacentsDuplicatesInString

---
+++ LeedCode.Test/Stack/RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LeedCode.Test.Stack
+{
+    public class RemoveAllAdjacentDuplicatesInString
+    {
+        public string RemoveDuplicates(string s)
+        {
+            Stack<char> stack = new();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(stack.Count > 0 && stack.Peek() == s[i])
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+
+            StringBuilder answer = new();
+
+            for(int i = stack.Count; i >= 1; i--)
+            {
+                answer.Append(stack.Pop());
+            }
+
+            return new string (answer.ToString().Reverse().ToArray());
+        }
+    }
+}
---
+++ LeedCode.Test/Stack/RemoveAllAdjacentDuplicatesInStringTest.cs
@@ -0,0 +1,29 @@
+﻿namespace LeedCode.Test.Stack
+{
+    public class RemoveAllAdjacentDuplicatesInStringTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            RemoveAllAdjacentDuplicatesInString remove = new();
+
+            string answer = remove.RemoveDuplicates("abbaca");
+
+            string expected = "ca";
+
+            Assert.Equal(expected, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            RemoveAllAdjacentDuplicatesInString remove = new();
+
+            string answer = remove.RemoveDuplicates("azxxzy");
+
+            string expected = "ay";
+
+            Assert.Equal(expected, answer);
+        }
+    }
+}
