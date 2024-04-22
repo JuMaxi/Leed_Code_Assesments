d69e38ed1b84a62145e4e947e3cc4d287cafe72d
JuMaxi <jmaximovski@gmail.com>
2024-04-22 19:27:32 +0100

    Working with Stack and Queue - NumberOfStudentsUnableToEatLunch

---
+++ LeedCode.Test/Stack/NumberOfStudentsUnableToEatLunch.cs
@@ -0,0 +1,55 @@
+﻿namespace LeedCode.Test.Stack;
+
+public class NumberOfStudentsUnableToEatLunch
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        Stack<int> stack = ConvertArraySandwichesToStack(sandwiches);
+        Queue<int> queue = ConvertArrayStudentsToQueue(students);
+
+        int throughQueue = 0;
+
+        while(stack.Count > 0 && queue.Count > throughQueue)
+        {
+            int firstStudent = queue.Dequeue();
+            int topSandwiche = stack.Peek();
+
+            if (firstStudent == topSandwiche)
+            {
+                stack.Pop();
+                throughQueue = 0;
+            }
+            else
+            {
+                queue.Enqueue(firstStudent);
+                throughQueue++;
+            }
+        }
+
+        return stack.Count;
+    }
+
+    private Stack<int> ConvertArraySandwichesToStack(int[] sandwiches)
+    {
+        Stack<int> stack = new();
+
+        for(int i = sandwiches.Length - 1; i >= 0; i--)
+        {
+            stack.Push(sandwiches[i]);
+        }
+
+        return stack;
+    }
+
+    private Queue<int> ConvertArrayStudentsToQueue(int[] students)
+    {
+        Queue<int> queue = new();
+
+        for(int i = 0;i < students.Length; i++)
+        {
+            queue.Enqueue(students[i]);
+        }
+
+        return queue;
+    }
+}
---
+++ LeedCode.Test/Stack/NumberOfStudentsUnableToEatLunchTest.cs
@@ -0,0 +1,31 @@
+﻿namespace LeedCode.Test.Stack
+{
+    public class NumberOfStudentsUnableToEatLunchTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            NumberOfStudentsUnableToEatLunch number = new();
+
+            int[] students = { 1, 1, 0, 0 };
+            int[] sandwiches = { 0, 1, 0, 1 };
+
+            int answer = number.CountStudents(students, sandwiches);
+
+            Assert.Equal(0, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            NumberOfStudentsUnableToEatLunch number = new();
+
+            int[] students = { 1, 1, 1, 0, 0, 1};
+            int[] sandwiches = { 1, 0, 0, 0, 1, 1 };
+
+            int answer = number.CountStudents(students, sandwiches);
+
+            Assert.Equal(3, answer);
+        }
+    }
+}
