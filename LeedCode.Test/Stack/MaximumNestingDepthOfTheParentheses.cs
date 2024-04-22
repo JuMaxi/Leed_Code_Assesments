ea5bc4f95f885cd93b7af362ff153e6bd34a7975
JuMaxi <jmaximovski@gmail.com>
2024-04-22 18:21:48 +0100

    Working with Stack - MaximumNestingDepthOfTheParentheses

---
+++ LeedCode.Test/Stack/MaximumNestingDepthOfTheParentheses.cs
@@ -0,0 +1,33 @@
+﻿namespace LeedCode.Test.Stack;
+
+public class MaximumNestingDepthOfTheParentheses
+{
+    public int MaxDepth(string s)
+    {
+        Stack<char> stack = new();
+        int answer = 0;
+        int temp = 0;
+
+        foreach(char c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(c);
+                temp++;
+            }
+            else
+            {
+                if (c == ')')
+                {
+                    stack.Pop();
+
+                    answer = Math.Max(temp, answer);
+
+                    temp = stack.Count;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ LeedCode.Test/Stack/MaximumNestingDepthOfTheParenthesesTest.cs
@@ -0,0 +1,29 @@
+﻿namespace LeedCode.Test.Stack
+{
+    public class MaximumNestingDepthOfTheParenthesesTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            MaximumNestingDepthOfTheParentheses maximum = new();
+
+            string s = "(1+(2*3)+((8)/4))+1";
+
+            int answer = maximum.MaxDepth(s);
+
+            Assert.Equal(3, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            MaximumNestingDepthOfTheParentheses maximum = new();
+
+            string s = "(1)+((2))+(((3)))";
+
+            int answer = maximum.MaxDepth(s);
+
+            Assert.Equal(3, answer);
+        }
+    }
+}
