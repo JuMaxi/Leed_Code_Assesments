002d92133b5dd9b90a196653d5184fe433db86b6
JuMaxi <jmaximovski@gmail.com>
2024-04-30 11:08:02 +0100

    Working with Binary Search - Search Integer

---
+++ LeedCode.Test/BinarySearch/SearchInteger.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace LeedCode.Test.BinarySearch
+{
+    public class SearchInteger
+    {
+        public int Search(int[] nums, int target)
+        {
+            int right = nums.Length - 1;
+            int left = 0;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                if(target > nums[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    if(target < nums[middle])
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        return middle;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeedCode.Test/BinarySearch/SearchIntegerTest.cs
@@ -0,0 +1,41 @@
+﻿namespace LeedCode.Test.BinarySearch
+{
+    public class SearchIntegerTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            SearchInteger search = new();
+            int[] nums = { -1, 0, 3, 5, 9, 12 };
+            int target = 9;
+
+            int answer = search.Search(nums, target);
+
+            Assert.Equal(4, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            SearchInteger search = new();
+            int[] nums = { -1, 0, 3, 5, 9, 12 };
+            int target = 2;
+
+            int answer = search.Search(nums, target);
+
+            Assert.Equal(-1, answer);
+        }
+
+        [Fact]
+        public void Test03() 
+        {
+            SearchInteger search = new();
+            int[] nums = { 5 };
+            int target = 5;
+
+            int answer = search.Search(nums, target);
+
+            Assert.Equal(0, answer);
+        }
+    }
+}
