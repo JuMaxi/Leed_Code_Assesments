a4764ee7595efd390a9c943f4a764354983e1967
JuMaxi <jmaximovski@gmail.com>
2024-05-22 10:24:49 +0100

    Working with Linked List with a recursive function - merging a sorted linked list.

---
+++ LeedCode.Test/LinkedList/MergeTwoSortedLists.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions.Equivalency;
+using System.ComponentModel.Design;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class MergeTwoSortedLists
+    {
+        public Node? mergedList;
+
+        public Node MergeTwoLists(Node list1, Node list2)
+        {
+            if (VerifyNextIsNotNullAndDataIsLessOrEqual(list1, list2))
+            {
+                mergedList = CallFunctions(list1, list2);
+            }
+            else
+            {
+                if (VerifyNextIsNotNullAndDataIsLessOrEqual(list2, list1))
+                {
+                    mergedList = CallFunctions(list2, list1);
+                }
+            }
+
+            mergedList = SaveMergedListIfNextIsNull(list1, list2, mergedList);
+
+            return mergedList;
+        }
+
+        private Node SaveMergedListIfNextIsNull(Node list1, Node list2, Node mergedList)
+        {
+            if (VerifyNextIsNullAndDataIsLessOrEqual(list1, list2))
+            {
+                mergedList = SaveMergedList(list1, mergedList);
+            }
+            if (VerifyNextIsNullAndDataIsLessOrEqual(list2, list1))
+            {
+                mergedList = SaveMergedList(list2, mergedList);
+            }
+
+            return mergedList;
+        }
+        private bool VerifyNextIsNullAndDataIsLessOrEqual(Node list1, Node list2)
+        {
+            if (list1.Next is null && list1.Data <= list2.Data)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private Node SaveMergedList(Node list, Node MergedList)
+        {
+            Node newNode = new();
+            newNode.Data = list.Data;
+            newNode.Next = mergedList;
+            mergedList = newNode;
+
+            return mergedList;
+        }
+
+        private bool VerifyNextIsNotNullAndDataIsLessOrEqual(Node list1, Node list2)
+        {
+            if (list1.Next is not null && list1.Data <= list2.Data)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private Node CallFunctions(Node list1, Node list2)
+        {
+            MergeTwoLists(list1.Next, list2);
+            mergedList = SaveMergedList(list1, mergedList);
+
+            return mergedList;
+        }
+
+
+
+    }
+}
---
+++ LeedCode.Test/LinkedList/MergeTwoSortedListsTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class MergeTwoSortedListsTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node list1 = new Node();
+            list1.Data = 1;
+            list1.Add(2);
+            list1.Add(4);
+
+            Node list2 = new Node();
+            list2.Data = 1;
+            list2.Add(3);
+            list2.Add(4);
+
+            MergeTwoSortedLists merge = new();
+            Node answer = merge.MergeTwoLists(list1, list2);
+
+            Node expected = new();
+            expected.Data = 1;
+            expected.Add(1);
+            expected.Add(2);
+            expected.Add(3);
+            expected.Add(4);
+            expected.Add(4);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+    }
+}
