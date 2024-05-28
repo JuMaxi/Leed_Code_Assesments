ae89dce3839f61c4b450656dca02d38886135ed1
JuMaxi <jmaximovski@gmail.com>
2024-05-28 14:26:25 +0100

    Working with a linked list and a recursive function - Converting binary numbers to decimal.

---
+++ LeedCode.Test/LinkedList/ConvertBinaryNumberToInteger.cs
@@ -0,0 +1,38 @@
+﻿namespace LeedCode.Test.LinkedList
+{
+    public class Result
+    {
+        public int Sum { get; set; }
+        public int Position { get; set; }
+    }
+    public class ConvertBinaryNumberToInteger
+    {
+        public int GetDecimalValue(Node head)
+        {
+            return Recurse(head).Sum;
+        }
+
+        private Result Recurse(Node head)
+        {
+            if (head.Next is null)
+            {
+                return new Result() { Position = 1, Sum = Power(0) * head.Data };
+            }
+            else
+            {
+                var nextResult = Recurse(head.Next);
+                return new Result()
+                {
+                    Position = nextResult.Position + 1,
+                    Sum = nextResult.Sum + (Power(nextResult.Position) * head.Data)
+                };
+            }
+        }
+
+        private int Power(int factor)
+        {
+            
+            return (int)Math.Pow(2, factor);
+        }
+    }
+}
---
+++ LeedCode.Test/LinkedList/ConvertBinaryNumberToIntegerTest.cs
@@ -0,0 +1,33 @@
+﻿namespace LeedCode.Test.LinkedList
+{
+    public class ConvertBinaryNumberToIntegerTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(0);
+            head.Add(1);
+
+            ConvertBinaryNumberToInteger convert = new();
+
+            int answer = convert.GetDecimalValue(head);
+
+            Assert.Equal(5, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 0;
+
+            ConvertBinaryNumberToInteger convert = new();
+
+            int answer = convert.GetDecimalValue(head);
+
+            Assert.Equal(0, answer);
+        }
+    }
+}
