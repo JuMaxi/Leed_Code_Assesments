0a7980fabb8e3c78ecbea14988cf8ba79f3a4ffe
JuMaxi <jmaximovski@gmail.com>
2024-05-30 14:41:32 +0100

    In this assessment, I am practicing linked lists and recursive functions, looking for the middle of the linked list and returning it and the values that follow it.

---
+++ LeedCode.Test/LinkedList/MiddleOfTheLinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace LeedCode.Test.LinkedList;
+
+public class MiddleOfTheLinkedList
+{
+    public class PositionAndNodes
+    {
+        public int Position { get; set; }
+        public Node Head { get; set; }
+    }
+
+    public Node MiddleNode(Node head)
+    {
+        return CountNodes(head, 1).Head;
+    }
+
+    private PositionAndNodes CountNodes(Node head, int Position)
+    {
+        if(head.Next is null)
+        {
+            Node node = new() { Data = head.Data };
+            return new PositionAndNodes { Position = Position, Head = node};
+        }
+        else
+        {
+            var result = CountNodes(head.Next, Position + 1);
+
+            if(Position > result.Position/2)
+            {
+                Node node = new() { Data = head.Data, Next = result.Head };
+                result.Head = node;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeedCode.Test/LinkedList/MiddleOfTheLinkedListTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class MiddleOfTheLinkedListTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(2);
+            head.Add(3);
+            head.Add(4);
+            head.Add(5);
+
+            MiddleOfTheLinkedList middle = new();
+            Node answer = middle.MiddleNode(head);
+
+            Node expected = new();
+            expected.Data = 3;
+            expected.Add(4);
+            expected.Add(5);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(2);
+            head.Add(3);
+            head.Add(4);
+            head.Add(5);
+            head.Add(6);
+
+            MiddleOfTheLinkedList middle = new();
+            Node answer = middle.MiddleNode(head);
+
+            Node expected = new();
+            expected.Data = 4;
+            expected.Add(5);
+            expected.Add(6);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+    }
+}
