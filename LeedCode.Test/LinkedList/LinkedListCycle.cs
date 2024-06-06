d322e4ba1b1f6ce927578e97d951d1a4ef693b35
JuMaxi <jmaximovski@gmail.com>
2024-06-06 13:52:08 +0100

    To verifying if there is a cycle-linked list, I used a hash table to resolve this problem and ran it in O(1) time.

---
+++ LeedCode.Test/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,23 @@
+﻿namespace LeedCode.Test.LinkedList
+{
+    public class LinkedListCycle
+    {
+        public bool HasCycle(Node head)
+        {
+            Dictionary<Node, int> dict = new();
+            while(head != null)
+            {
+                if (!dict.ContainsKey(head))
+                {
+                    dict.Add(head, 1);
+                    head = head.Next;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ LeedCode.Test/LinkedList/LinkedListCycleTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class LinkedListCycleTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node node1 = new();
+            Node node2 = new();
+            Node node3 = new();
+            Node node4 = new();
+
+            node1.Data = 3;
+            node2.Data = 2;
+            node3.Data = 0;
+            node4.Data = -4;
+
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            node4.Next = node2;
+
+            LinkedListCycle cycle = new();
+            bool answer = cycle.HasCycle(node1);
+
+            answer.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node node1 = new();
+            Node node2 = new();
+            node1.Data = 1;
+            node2.Data = 2;
+            node1.Next = node2;
+            node2.Next = node1;
+
+            LinkedListCycle cycle = new();
+            bool answer = cycle.HasCycle(node1);
+
+            answer.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            Node node1 = new();
+            node1.Data = 1;
+
+            LinkedListCycle cycle = new();
+            bool answer = cycle.HasCycle(node1);
+
+            answer.Should().BeFalse();
+
+        }
+    }
+}
