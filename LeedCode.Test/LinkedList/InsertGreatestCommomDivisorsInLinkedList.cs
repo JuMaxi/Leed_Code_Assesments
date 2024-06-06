f4c48db22824e03ad62f31bdfc3a7b4a7118a7c1
JuMaxi <jmaximovski@gmail.com>
2024-06-06 10:58:40 +0100

    Inserting the greatest common divisor between each 2 elements in a single linked list.

---
+++ LeedCode.Test/LinkedList/InsertGreatestCommomDivisorsInLinkedList.cs
@@ -0,0 +1,41 @@
+﻿namespace LeedCode.Test.LinkedList;
+
+public class InsertGreatestCommomDivisorsInLinkedList
+{
+    public Node InsertGreatestCommonDivisors(Node head)
+    {
+        Node newHead = head;
+        while(head.Next != null)
+        {
+            int answer = 1;
+
+            int smaller = Math.Min(head.Data, head.Next.Data);
+            int currentData = head.Data;
+            int nextData = head.Next.Data;
+
+            for(int i = 2; i <= smaller; i++)
+            {
+                if(currentData % i == 0 && nextData % i == 0)
+                {
+                    answer *= i;
+                    currentData = currentData / i;
+                    nextData = nextData / i;
+
+                    if(currentData == 1 || nextData == 1)
+                    {
+                        break;
+                    }
+
+                    i--;
+                }
+            }
+            Node node = new();
+            node.Data = answer;
+            node.Next = head.Next;
+            head.Next = node;
+
+            head = head.Next.Next;
+        }
+        return newHead;
+    }
+}
---
+++ LeedCode.Test/LinkedList/InsertGreatestCommomDivisorsInLinkedListTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class InsertGreatestCommomDivisorsInLinkedListTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 18;
+            head.Add(6);
+            head.Add(10);
+            head.Add(3);
+
+            InsertGreatestCommomDivisorsInLinkedList insert = new();
+            Node answer = insert.InsertGreatestCommonDivisors(head);
+
+            Node expected = new();
+            expected.Data = 18;
+            expected.Add(6);
+            expected.Add(6);
+            expected.Add(2);
+            expected.Add(10);
+            expected.Add(1);
+            expected.Add(3);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 7;
+
+            InsertGreatestCommomDivisorsInLinkedList insert = new();
+            Node answer = insert.InsertGreatestCommonDivisors(head);
+
+            Node expected = new();
+            expected.Data = 7;
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+    }
+}
