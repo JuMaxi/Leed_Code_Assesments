8366ed6d6945197d757898615abf2f7c48b36baa
JuMaxi <jmaximovski@gmail.com>
2024-06-06 11:31:28 +0100

    Removing duplicate elements into a sorted linked list and returning the new sorted linked list.

---
+++ LeedCode.Test/LinkedList/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,21 @@
+﻿namespace LeedCode.Test.LinkedList;
+
+public class RemoveDuplicatesFromSortedList
+{
+    public Node DeleteDuplicates(Node head)
+    {
+        Node newHead = head;
+
+        while(head != null)
+        {
+            while(head.Next != null && head.Data == head.Next.Data)
+            {
+                head.Next = head.Next.Next;
+            }
+
+            head = head.Next;
+        }
+
+        return newHead;
+    }
+}
---
+++ LeedCode.Test/LinkedList/RemoveDuplicatesFromSortedListTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class RemoveDuplicatesFromSortedListTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(1);
+            head.Add(2);
+
+            RemoveDuplicatesFromSortedList remove = new();
+            Node answer = remove.DeleteDuplicates(head);
+
+            Node expected = new();
+            expected.Data = 1;
+            expected.Add(2);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(1);
+            head.Add(2);
+            head.Add(3);
+            head.Add(3);
+
+            RemoveDuplicatesFromSortedList remove = new();
+            Node answer = remove.DeleteDuplicates(head);
+
+            Node expected = new();
+            expected.Data = 1;
+            expected.Add(2);
+            expected.Add(3);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+    }
+}
