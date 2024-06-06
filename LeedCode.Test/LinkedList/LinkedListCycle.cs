24c0f5d9d64ea74995c20f58d43f4a8b53e96fc2
JuMaxi <jmaximovski@gmail.com>
2024-06-06 14:12:24 +0100

    verifying a cycle-linked list (the same previous problem).

--- LeedCode.Test/LinkedList/LinkedListCycle.cs
+++ LeedCode.Test/LinkedList/LinkedListCycle.cs
@@ -1,23 +1,22 @@
-﻿namespace LeedCode.Test.LinkedList
+﻿namespace LeedCode.Test.LinkedList;
+
+public class LinkedListCycle
 {
-    public class LinkedListCycle
+    public bool HasCycle(Node head)
     {
-        public bool HasCycle(Node head)
+        Dictionary<Node, int> dict = new();
+        while(head != null)
         {
-            Dictionary<Node, int> dict = new();
-            while(head != null)
+            if (!dict.ContainsKey(head))
             {
-                if (!dict.ContainsKey(head))
-                {
-                    dict.Add(head, 1);
-                    head = head.Next;
-                }
-                else
-                {
-                    return true;
-                }
+                dict.Add(head, 1);
+                head = head.Next;
             }
-            return false;
+            else
+            {
+                return true;
+            }
         }
+        return false;
     }
 }
