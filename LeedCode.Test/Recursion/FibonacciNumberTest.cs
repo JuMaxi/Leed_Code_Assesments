80d6dfd5aec80e25e0130be61ad1ac78f03f1652
JuMaxi <jmaximovski@gmail.com>
2024-06-11 17:06:31 +0100

    It uses recursion to solve a Fibonacci sequence.

---
+++ LeedCode.Test/Recursion/FibonacciNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode.Test.Recursion
+{
+    public class FibonacciNumberTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            FibonacciNumber fibonacci = new();
+            int answer = fibonacci.Fib(2);
+
+            Assert.Equal(1, answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            FibonacciNumber fibonacci = new();
+            int answer = fibonacci.Fib(3);
+
+            Assert.Equal(2, answer);
+        }
+    }
+}
