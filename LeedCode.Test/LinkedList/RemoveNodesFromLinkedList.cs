9d792a234ab40f4f365b185f92a85f030ec9cc16
JuMaxi <jmaximovski@gmail.com>
2024-06-11 14:22:36 +0100

    It uses a recursive function to remove a node in a single linked list if it is a greater value anywhere on the right side of it.

---
+++ LeedCode.Test/LinkedList/RemoveNodesFromLinkedList.cs
@@ -0,0 +1,26 @@
+﻿namespace LeedCode.Test.LinkedList;
+
+public class RemoveNodesFromLinkedList
+{
+    public Node RemoveNodes(Node head)
+    {
+        if (head.Next == null)
+        {
+            return new Node { Data = head.Data };
+        }
+        else
+        {
+            var result = RemoveNodes(head.Next);
+
+            if (head.Data >= result.Data)
+            {
+                Node tempNode = new();
+                tempNode.Data = head.Data;
+                tempNode.Next = result;
+
+                result = tempNode;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeedCode.Test/LinkedList/RemoveNodesFromLinkedListTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.LinkedList
+{
+    public class RemoveNodesFromLinkedListTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 5;
+            head.Add(2);
+            head.Add(13);
+            head.Add(3);
+            head.Add(8);
+
+            RemoveNodesFromLinkedList remove = new();
+            Node answer = remove.RemoveNodes(head);
+
+            Node expected = new();
+            expected.Data = 13;
+            expected.Add(8);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(1);
+            head.Add(1);
+            head.Add(1);
+
+            RemoveNodesFromLinkedList remove = new();
+            Node answer = remove.RemoveNodes(head);
+
+            Node expected = new();
+            expected.Data = 1;
+            expected.Add(1);
+            expected.Add(1);
+            expected.Add(1);
+
+            answer.Should().BeEquivalentTo(expected);
+        }
+    }
+}
