6680abdc462c56b06fcdbd4499b0bf35444aeb2f
JuMaxi <jmaximovski@gmail.com>
2024-06-12 16:47:31 +0100

    In this assessment, I am using a recursive function to verify whether a number is a power of two.

---
+++ LeedCode.Test/Recursion/PowerOfTwo.cs
@@ -0,0 +1,28 @@
+﻿namespace LeedCode.Test.Recursion
+{
+    public class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 0)
+            {
+                return false;
+            }
+
+            if (n == 1)
+            {
+                return true;
+            }
+            else
+            {
+                bool answer = IsPowerOfTwo(n / 2);
+
+                if(n % 2 != 0)
+                {
+                    return false;
+                }
+                return answer;
+            }
+        }
+    }
+}
---
+++ LeedCode.Test/Recursion/PowerOfTwoTest.cs
@@ -0,0 +1,33 @@
+﻿namespace LeedCode.Test.Recursion
+{
+    public class PowerOfTwoTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            PowerOfTwo power = new();
+            bool answer = power.IsPowerOfTwo(1);
+
+            Assert.True(answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            PowerOfTwo power = new();
+            bool answer = power.IsPowerOfTwo(16);
+
+            Assert.True(answer);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            PowerOfTwo power = new();
+            bool answer = power.IsPowerOfTwo(3);
+
+            Assert.False(answer);
+        }
+
+    }
+}
