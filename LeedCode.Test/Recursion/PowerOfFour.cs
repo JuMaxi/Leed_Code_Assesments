2c51bb7c5b1c2d8479a4c0f6eca0c8824e663980
JuMaxi <jmaximovski@gmail.com>
2024-06-12 16:12:10 +0100

    In this assessment, I am using a recursive function to verify whether a number is a power of four.

---
+++ LeedCode.Test/Recursion/PowerOfFour.cs
@@ -0,0 +1,29 @@
+﻿namespace LeedCode.Test.Recursion;
+
+public class PowerOfFour
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n == 0)
+        {
+            return false;
+        }
+
+        if (n == 1)
+        {
+            return true;
+        }
+        else
+        {
+            bool answer = IsPowerOfFour(n / 4);
+
+            if (n % 4 != 0)
+            {
+                return false;
+            }
+            return answer;
+        }
+    }
+
+
+}
---
+++ LeedCode.Test/Recursion/PowerOfFourTest.cs
@@ -0,0 +1,50 @@
+﻿namespace LeedCode.Test.Recursion
+{
+    public class PowerOfFourTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            PowerOfFour power = new();
+            bool answer = power.IsPowerOfFour(16);
+
+            Assert.True(answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            PowerOfFour power = new();
+            bool answer = power.IsPowerOfFour(5);
+
+            Assert.False(answer);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            PowerOfFour power = new();
+            bool answer = power.IsPowerOfFour(1);
+
+            Assert.True(answer);
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            PowerOfFour power = new();
+            bool answer = power.IsPowerOfFour(-2147483648);
+
+            Assert.False(answer);
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            PowerOfFour power = new();
+            bool answer = power.IsPowerOfFour(0);
+
+            Assert.False(answer);
+        }
+    }
+}
