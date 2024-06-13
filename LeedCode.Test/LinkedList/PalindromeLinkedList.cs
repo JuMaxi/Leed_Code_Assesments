f4caa4d172a0c521364ebea5d7205ada86f8c14a
JuMaxi <jmaximovski@gmail.com>
2024-06-13 19:05:22 +0100

    In this assessment, I am using a recursive function to verify if a single linked list is a Palindrome.

--- LeedCode.Test/LinkedList/PalindromeLinkedList.cs
+++ LeedCode.Test/LinkedList/PalindromeLinkedList.cs
@@ -2,27 +2,41 @@
 
 public class PalindromeLinkedList
 {
+    public class ReturnNodes
+    {
+        public bool Palindrome { get; set; }
+        public Node Node { get; set; }
+    }
     public bool IsPalindrome(Node head)
     {
-        Stack<int> stack = new();
         Node temp = head;
+        return CreateNewNode(head, temp).Palindrome;
+    }
 
-        while(head != null)
+    private ReturnNodes CreateNewNode(Node head, Node temp)
+    {
+        if (head is null)
         {
-            stack.Push(head.Data);
-            head = head.Next;
+            return new ReturnNodes { Palindrome = true, Node = temp};
         }
-
-        while(temp != null)
+        else
         {
-            if(temp.Data != stack.Pop())
-            {
-                return false;
+            var answer = CreateNewNode(head.Next, temp);
+
+            if(answer.Palindrome is false) 
+            { 
+                return answer;
             }
-            //stack.Pop();
-            temp = temp.Next;
-        }
 
-        return true;
+            if(answer.Node.Data != head.Data)
+            {
+                answer.Palindrome = false;
+                return answer;
+            }
+
+            answer.Node = answer.Node.Next;
+
+            return answer;
+        }
     }
 }
