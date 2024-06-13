26a62bb8a03e67e49149753ae829993856d6a559
JuMaxi <jmaximovski@gmail.com>
2024-06-13 16:05:50 +0100

    In this assessment I am using a stack to verify if a single linked list is a palindrome.

---
+++ LeedCode.Test/LinkedList/PalindromeLinkedList.cs
@@ -0,0 +1,28 @@
+﻿namespace LeedCode.Test.LinkedList;
+
+public class PalindromeLinkedList
+{
+    public bool IsPalindrome(Node head)
+    {
+        Stack<int> stack = new();
+        Node temp = head;
+
+        while(head != null)
+        {
+            stack.Push(head.Data);
+            head = head.Next;
+        }
+
+        while(temp != null)
+        {
+            if(temp.Data != stack.Pop())
+            {
+                return false;
+            }
+            //stack.Pop();
+            temp = temp.Next;
+        }
+
+        return true;
+    }
+}
---
+++ LeedCode.Test/LinkedList/PalindromeLinkedListTest.cs
@@ -0,0 +1,47 @@
+﻿namespace LeedCode.Test.LinkedList
+{
+    public class PalindromeLinkedListTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(2);
+            head.Add(2);
+            head.Add(1);
+
+            PalindromeLinkedList palindrome = new();
+            bool answer = palindrome.IsPalindrome(head);
+
+            Assert.True(answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(2);
+
+            PalindromeLinkedList palindrome = new();
+            bool answer = palindrome.IsPalindrome(head);
+
+            Assert.False(answer);
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            Node head = new();
+            head.Data = 1;
+            head.Add(0);
+            head.Add(1);
+
+            PalindromeLinkedList palindrome = new();
+            bool answer = palindrome.IsPalindrome(head);
+
+            Assert.True(answer);
+        }
+    }
+}
