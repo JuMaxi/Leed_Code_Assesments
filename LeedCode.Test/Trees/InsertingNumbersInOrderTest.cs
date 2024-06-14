ba27370634363189c7d2dff4de22feb2dd640f3f
JuMaxi <jmaximovski@gmail.com>
2024-06-14 18:10:42 +0100

    Creating a class node to work with a Tree and using a recursive function to create a Binary Search Tree.

---
+++ LeedCode.Test/Trees/InsertingNumbersInOrderTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.Trees
+{
+    public class InsertingNumbersInOrderTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 3;
+
+            head.Add(5);
+            head.Add(8);
+            head.Add(7);
+            head.Add(6);
+            head.Add(1);
+            head.Add(2);
+            head.Add(-5);
+            head.Add(3);
+
+            head.Right.Data.Should().Be(5); 
+            head.Right.Right.Data.Should().Be(8); 
+            head.Right.Right.Left.Data.Should().Be(7);
+            head.Right.Right.Left.Left.Data.Should().Be(6);
+
+            head.Left.Data.Should().Be(1);
+            head.Left.Right.Data.Should().Be(2);
+            head.Left.Right.Right.Data.Should().Be(3);
+            head.Left.Left.Data.Should().Be(-5);
+
+
+        }
+    }
+}
---
+++ LeedCode.Test/Trees/Node.cs
@@ -0,0 +1,43 @@
+﻿namespace LeedCode.Test.Trees
+{
+    public class Node
+    {
+        public int Data {  get; set; }
+        public Node? Right { get; set; }
+        public Node? Left { get; set; }
+
+        public void Add(int number)
+        { 
+            if(number > Data)
+            {
+                if(Right == null)
+                {
+                    Node node = new();
+                    node.Data = number;
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(number);
+                }
+                
+            }
+            else
+            {
+                if(Left == null)
+                {
+                    Node node = new();
+                    node.Data = number;
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(number);
+                }
+            }
+           
+        
+        
+        }
+    }
+}
