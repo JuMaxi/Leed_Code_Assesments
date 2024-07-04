27022b09e5631a5f0a15d1965c99c32a1f292f03
JuMaxi <jmaximovski@gmail.com>
2024-07-04 17:30:05 +0100

    Creating a recursive function to find a target into a binary search tree + unit tests.

---
+++ LeedCode.Test/Trees/SearchInABinaryTree.cs
@@ -0,0 +1,30 @@
+﻿namespace LeedCode.Test.Trees;
+
+public class SearchInABinaryTree
+{
+    public Node SearchBST(Node head, int val)
+    {
+        if (val == head.Data)
+        {
+            return head;
+        }
+
+        if (val > head.Data)
+        {
+            if (head.Right != null)
+            {
+                return SearchBST(head.Right, val);
+            }
+        }
+
+        if (val < head.Data)
+        {
+            if (head.Left != null)
+            {
+                return SearchBST(head.Left, val);
+            }
+        }
+
+        return null;
+    }
+}
---
+++ LeedCode.Test/Trees/SearchInABinaryTreeTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.Trees
+{
+    public class SearchInABinaryTreeTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 4;
+            head.Add(7);
+            head.Add(2);
+            head.Add(1);
+            head.Add(3);
+
+            Node answer = new();
+            answer.Data = 2;
+            answer.Add(1);
+            answer.Add(3);
+
+            SearchInABinaryTree tree = new();
+            tree.SearchBST(head, 2).Should().BeEquivalentTo(answer);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 4;
+            head.Add(7);
+            head.Add(2);
+            head.Add(1);
+            head.Add(3);
+
+            Node answer = null;
+
+            SearchInABinaryTree tree = new();
+            tree.SearchBST(head, 5).Should().BeEquivalentTo(answer);
+        }
+    }
+}
