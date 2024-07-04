ae6edfcdc8c5956fea902bb84830bab5ebb8e151
JuMaxi <jmaximovski@gmail.com>
2024-07-04 15:42:09 +0100

    Creating a recursive function to verify if the sum of the children nodes are equal the data in the head node + unit tests.

---
+++ LeedCode.Test/Trees/RootEqualsSumOfChildren.cs
@@ -0,0 +1,28 @@
+﻿namespace LeedCode.Test.Trees;
+
+public class RootEqualsSumOfChildren
+{
+    public bool CheckTree(Node head)
+    {
+        if (SumChild(head) == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    private int SumChild(Node head)
+    {
+        int sum = head.Data;
+
+        if (head.Right != null)
+        {
+            sum -= SumChild(head.Right);
+        }
+        if (head.Left != null)
+        {
+            sum -= SumChild(head.Left);
+        }
+
+        return sum;
+    }
+}
---
+++ LeedCode.Test/Trees/RootEqualsSumOfChildrenTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace LeedCode.Test.Trees
+{
+    public class RootEqualsSumOfChildrenTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            Node head = new();
+            head.Data = 10;
+            
+            Node right = new();
+            right.Data = 6;
+            head.Right = right;
+
+            Node left = new();
+            left.Data = 4;
+            head.Left = left;
+
+            RootEqualsSumOfChildren root = new();
+            root.CheckTree(head).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            Node head = new();
+            head.Data = 5;
+
+            Node right = new();
+            right.Data = 1;
+            head.Right = right;
+
+            Node left = new();
+            left.Data = 3;
+            head.Left = left;
+
+            RootEqualsSumOfChildren root = new();
+            root.CheckTree(head).Should().BeFalse();
+        }
+    }
+}
